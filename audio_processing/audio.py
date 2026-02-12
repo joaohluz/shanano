518ddfa880c224ac6005999d26d50a21fac14b20
joaohluz <joao.luz@weareplanet.com>
2026-02-12 00:59:40 +0000

    Experimenting with mic audio processing steps

--- audio_processing/audio.py
+++ audio_processing/audio.py
@@ -1,6 +1,8 @@
+from scipy.signal import butter, filtfilt
 import librosa
-import sounddevice as sd
 import numpy as np
+import sounddevice as sd
+
 from config import DEFAULT_SR
 
 def load_audio(path, sr=DEFAULT_SR):
@@ -11,3 +13,28 @@ def record_audio(duration=7, sr=DEFAULT_SR):
     audio = sd.rec(int(duration * sr), samplerate=sr, channels=1)
     sd.wait()
     return audio.flatten(), sr
+
+def add_noise(y, noise_level=0.01):
+    noise = np.random.normal(0, noise_level, len(y))
+    return y + noise
+
+def apply_filter(data, fs, order=5, btype='low', cutoff=None, lowcut=None, highcut=None):
+    nyq = 0.5 * fs
+    if btype in ['low', 'high']:
+        normal_cutoff = cutoff / nyq
+        b, a = butter(order, normal_cutoff, btype=btype, analog=False)
+    elif btype == 'band':
+        low = lowcut / nyq
+        high = highcut / nyq
+        b, a = butter(order, [low, high], btype='band')
+    y = filtfilt(b, a, data)
+    return y
+
+def lowpass_filter(data, cutoff, fs, order=5):
+    return apply_filter(data, fs, order, 'low', cutoff=cutoff)
+
+def highpass_filter(data, cutoff, fs, order=5):
+    return apply_filter(data, fs, order, 'high', cutoff=cutoff)
+
+def bandpass_filter(data, lowcut, highcut, fs, order=5):
+    return apply_filter(data, fs, order, 'band', lowcut=lowcut, highcut=highcut)
